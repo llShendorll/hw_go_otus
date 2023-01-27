7d764dc048423b5902e673ef1eb8887ddb89ad72
shendor <webshendor@gmail.com>
2023-01-27 15:53:27 +0300

    Доработал задачу

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -17,21 +17,25 @@ func Top10(text string) []string {
 	text = strings.ToLower(text)
 	res := reg.ReplaceAllString(text, string(' '))
 	words := strings.Fields(res)
+	mapWords := make(map[string]int)
 
-	sort.Strings(words)
-	cnt := 1
-	list := make([]List, 0, len(words))
-	for i, word := range words {
-		if i+1 < len(words) {
-			if word == words[i+1] {
-				cnt++
-			} else {
-				list = append(list, List{word, cnt})
-				cnt = 1
-			}
+	for _, word := range words {
+		if mapWords[word] != 0 {
+			mapWords[word]++
+		} else {
+			mapWords[word] = 1
 		}
 	}
-	sort.SliceStable(list, func(i int, j int) bool {
+
+	list := make([]List, 0, len(mapWords))
+	for w, c := range mapWords {
+		list = append(list, List{w, c})
+	}
+
+	sort.Slice(list, func(i int, j int) bool {
+		if list[i].count == list[j].count {
+			return list[i].word < list[j].word
+		}
 		return list[i].count > list[j].count
 	})
 
